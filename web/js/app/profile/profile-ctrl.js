ada4c28232958a21a8752702632829d0f70c0c54
Sam Clarke <samclarke.g@gmail.com>
2014-06-02 20:31:34 -0700

    add restriction input to profile

--- web/js/app/profile/profile-ctrl.js
+++ web/js/app/profile/profile-ctrl.js
@@ -3,5 +3,14 @@ munchApp
 .controller('ProfileCtrl', function ($scope, $rootScope, MunchService, user) {
     console.log('user!', user.data);
 
-    if (!$rootScope.user) $scope.user = user.data;
+    if (!$rootScope.user) $rootScope.user = user.data;
+
+    $scope.severities = [{name: 'allergy'}, {name: 'preference'}];
+    $scope.newRestriction = {};
+
+    $scope.addRestriction = function (obj) {
+        //var newRestriction = {name: obj.}
+        $rootScope.user.restrictions.push(obj);
+        $scope.newRestriction = {};
+    }
 });
